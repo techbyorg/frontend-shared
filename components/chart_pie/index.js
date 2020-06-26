9f673a9ceb9935e8de59fce5b7e30333f6151b21
austinhallock <austinhallock@gmail.com>
2020-06-26 15:10:17 -0500

    charts

---
+++ components/chart_pie/index.js
@@ -0,0 +1,25 @@
+import { z, lazy, Suspense, Boundary } from 'zorium'
+
+import $spinner from '../spinner'
+
+const $pie = lazy(() => import(/* webpackChunkName: "nivo" */'@nivo/pie').then(({ ResponsivePie }) => ResponsivePie))
+
+if (typeof window !== 'undefined') { require('./index.styl') }
+
+export default function $chartPie ({ data, colors }) {
+  return z('.z-chart-pie', [
+    (typeof window !== 'undefined') &&
+      z(Boundary, { fallback: z('.error', 'err') }, [
+        z(Suspense, { fallback: $spinner }, [
+          z($pie, {
+            data,
+            innerRadius: 0.5,
+            colors,
+            enableRadialLabels: false,
+            enableSlicesLabels: false
+          }
+          )
+        ])
+      ])
+  ])
+}
